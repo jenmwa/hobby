0abb660b8e6f996da5b40181b34858f91d421c6c
jenmwa <jenmwa@gmail.com>
2023-02-24 14:56:17 +0100

    cherry-pick contactSection added

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,59 @@ import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <nav>some navigation
+    <ul>
+      <li>maybe a link</li>
+      <li>maybe two?</li>
+    </ul>
+  </nav>
+  <main>
+    ${aboutSection}
+    ${sortSection}
+    <div class="container">
+      ${imgSection}
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+    ${createImgPopup}
+    <section class="tech-container">
+      <h4>Lights, CAMERA, actions</h4>
+      <p>techSection</p>
+    </section>
+    <section class="tech-container">
+      <p>contactSection</p>
+    </section>
+  </main>
+
+  <footer>here's the footer</footer>
+`;
+
+// const zoomBtns: NodeListOf<Element> = document.querySelectorAll('.zoom-text');
+const nextBtn: HTMLButtonElement | null = document.getElementById('next-btn') as HTMLButtonElement;
+const prevBtn: HTMLButtonElement | null = document.getElementById('prev-btn') as HTMLButtonElement;
+
+const allImages = document.querySelectorAll<HTMLButtonElement>('.img-container');
+const imageView: HTMLElement | null = document.querySelector('.image-view');
+const imageBox: HTMLElement | null = document.querySelector('.image-box');
+
+let currentImageIndex = 0;
+
+function renderImgSection() {
+  let imgSection = '';
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+
+    imgSection += `
+      <article>
+        <button class="img-container">
+          <img class="img" src="${image.url}" alt="${image.alt}" width="300">
+          <div class="content">
+            <h2>${image.title}</h2>
+            <p>#${image.category}</p>
+          </div>
+        </button>
+      </article>
+  `;
+  }
+  return imgSection;
+}
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
