67928f9cec1117098e720bfccb48414719c03644
jenmwa <jenmwa@gmail.com>
2023-03-28 22:47:49 +0200

    :bug: imgpath

--- src/imgArray.ts
+++ src/imgArray.ts
@@ -8,7 +8,7 @@ interface Image {
 
   const images: Image[] = [
     {
-      url: '/myles-kennedy_photoBy_Jenny-Waller.webp',
+      url: 'img/myles-kennedy_photoBy_Jenny-Waller.webp',
       alt: 'Alter Bridge live photo by Jenny Waller',
       title: 'Alter Bridge',
       category: 'music',
